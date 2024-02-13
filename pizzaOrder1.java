04595d54af5a2b4229dfa9739fd1ba50a9cc7f84
Lucifer <vivekrai3944@gmail.com>
2024-02-13 12:32:13 +0530

    update

--- pizzaOrder1.java
+++ pizzaOrder1.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
-public class PizzaOrder1 {
+public class pizzaOrder1 {
+
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
 
-        
         System.out.println("Enter your customer name:");
         String Cname = scanner.nextLine();
-        
+
         System.out.println("Enter your customer Address:");
         String address = scanner.nextLine();
-        
+
         System.out.println("Enter your Mobile number:");
         long mobileNumber = scanner.nextLong();
-
 
         System.out.println("Choose a pizza: \n 1. Margherita,\n 2. Pepperoni,\n 3. Veggie,\n 4. BBQ Chicken, \n 5. Hawaiian");
         int pizzaChoice = scanner.nextInt();
 
-        System.out.println("Price");
+        System.out.println("Enter price:");
         int price = scanner.nextInt();
- 
 
         System.out.println("Enter quantity:");
         int quantity = scanner.nextInt();
@@ -29,58 +26,65 @@ public class PizzaOrder1 {
         System.out.println("Enter discount:");
         int discount = scanner.nextInt();
 
-        System.out.println("Tax");
-        double tax=scanner.nextDouble();
-
-     
-      
+        System.out.println("Enter tax:");
+        double tax = scanner.nextDouble();
 
         double totalPrice = 0;
 
         switch (pizzaChoice) {
             case 1:
-                System.out.println("You chose Margherita.");
-                totalPrice = quantity * price;
+                totalPrice = Margherita(quantity, price);
                 break;
             case 2:
-                System.out.println("You chose Pepperoni.");
-                totalPrice = quantity * price;
+                totalPrice = Pepperoni(quantity, price);
                 break;
             case 3:
-                System.out.println("You chose Veggie.");
-                totalPrice = quantity * price;
+                totalPrice = Veggie(quantity, price);
                 break;
             case 4:
-                System.out.println("You chose BBQ Chicken.");
-                totalPrice = quantity * price;
+                totalPrice = BBQChicken(quantity, price);
                 break;
             case 5:
-                System.out.println("You chose Hawaiian.");
-                totalPrice = quantity * price;
+                totalPrice = Hawaiian(quantity, price);
                 break;
             default:
                 System.out.println("Invalid choice. Please select a valid pizza.");
                 return;
         }
 
-        System.out.println("Costumer name="+Cname);
-        System.out.println("Address="+address);
-        System.out.println("Costumer mobile number="+mobileNumber);
+        System.out.println("Customer name: " + Cname);
+        System.out.println("Address: " + address);
+        System.out.println("Customer mobile number: " + mobileNumber);
         System.out.println("Quantity: " + quantity);
-        System.out.println("price " + price);
+        System.out.println("Price: $" + price);
         System.out.println("Total Price: $" + totalPrice);
-        System.out.println("Discount $" +discount);
-        System.out.println("netamout"+(discount- totalPrice));
-        System.out.println("tax"+tax);
+        System.out.println("Discount: $" + discount);
+        System.out.println("Net amount: $" + (totalPrice - discount));
+        System.out.println("Tax: $" + (totalPrice * tax / 100));
+    }
 
+    public static double Margherita(int quantity, int price) {
+        System.out.println("You chose Margherita.");
+        return quantity * price;
+    }
 
-       
-        System.out.println("netamount");
-       double netamount = scanner.nextDouble();
+    public static double Pepperoni(int quantity, int price) {
+        System.out.println("You chose Pepperoni.");
+        return quantity * price;
+    }
 
-    
+    public static double Veggie(int quantity, int price) {
+        System.out.println("You chose Veggie.");
+        return quantity * price;
+    }
 
-        System.out.println(netamount*tax/100);
+    public static double BBQChicken(int quantity, int price) {
+        System.out.println("You chose BBQ Chicken.");
+        return quantity * price;
+    }
 
+    public static double Hawaiian(int quantity, int price) {
+        System.out.println("You chose Hawaiian.");
+        return quantity * price;
     }
 }
